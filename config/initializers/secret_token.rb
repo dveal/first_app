8999c2f49279dc2f47d7249c65dc804a6a32da99
slowphoton@gmail.com <slowphoton@gmail.com>
2014-02-07 12:33:15 +0000

    Initialize depository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '64812cfbcd6349827997ef0b158840b1dd11755a2d79cd0acf477b591b474b9a3a3f5835f6a1820544355fd33f949272ab743a520a05345265d322025d5b8c4a'
